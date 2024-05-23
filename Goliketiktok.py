d9ed276333aa39a152057f6b47156b5dc1c7f19f
thanhbtm1 <153441968+thanhbtm1@users.noreply.github.com>
2024-05-23 09:19:55 +0700

    Update Goliketiktok.py

--- Goliketiktok.py
+++ Goliketiktok.py
@@ -195,7 +195,7 @@ while True:
       print("                                                    ",end = "\r")
       for x in chuoi:
         print(x,end = "")
-        sleep(0.05)
+        sleep(0.1)
       print("")  
     else:
       #print(nhantien)
